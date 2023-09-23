edfc9ff80189c9c6119e31501c3c4990d0f1c556
EshuShango <tobiakinyemi21@gmail.com>
2023-09-22 21:38:06 -0500

    Setup json package dependencies, need to begin user schema

---
+++ src/server/services/user_server/api/db/userSchema.js
@@ -0,0 +1,9 @@
+"use strict";
+/* Hey Copilot!
+   - Your a senior developer and you know what you are doing.
+   - Im working on the Backend of the application.
+   - Im using a architecture pattern called Microservices.
+   - help me build an express server.
+   - Im using vanilla js. 
+   - I need you to help me build a server that will create, update, delete, and verify a user.
+*/
